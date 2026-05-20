d733f61ff57fd0fef01a8f08b0256b36c5aea86d
phamdonganh0504 <phamdonganh180504@gmail.com>
2026-05-21 00:11:55 +0700

    chuc nang dang nhap

--- HeThongQuanLiSinhVien/LoginForm.cs
+++ HeThongQuanLiSinhVien/LoginForm.cs
@@ -15,14 +15,18 @@ namespace HeThongQuanLiSinhVien
             string username = txtUsername.Text;
             string password = txtPassword.Text;
 
-            if (username == "68PM2_phamdonganh_0001668@gmail.com" && password == "123456")
+            if (username == "admin@gmail.com" && password == "123456")
             {
                 MessageBox.Show("Đăng nhập thành công!");
                 // Mở frm_main và ẩn LoginForm
                 frm_main main = new frm_main();
-                main.Show();
                 this.Hide();
-            }
+
+                frm_main main = new frm_main();
+                main.FormClosed += (s, args) => this.Close();
+
+                main.Show();
+            }   
             else
             {
                 MessageBox.Show("Đăng nhập thất bại!");
